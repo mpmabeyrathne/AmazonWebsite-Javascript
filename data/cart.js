e25c1ed97733e8c63864a6ad27be60f230dd4b50
mpmabeyrathne <pasindumaduwantha2019@gmail.com>
2023-09-12 11:46:52 +0530

    sava data to locall storage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -25,6 +33,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function deleteCartItem(productId) {
@@ -36,4 +45,5 @@ export function deleteCartItem(productId) {
   });
 
   cart = newCart;
+  saveToStorage();
 }
